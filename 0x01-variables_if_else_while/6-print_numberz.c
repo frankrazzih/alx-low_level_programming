017a9ed311d9903bde30cb43569f24d7fa999f05
Francis Chege <francischege602@gmail.com>
2023-06-16 14:08:32 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * description - print a -z
+ * return - 0(success)
+ */
+int main(void){
+int alpha = 0;
+int omega = 9;
+        while (alpha <= omega){
+                       putchar(alpha + '0');
+                        alpha++;
+                        }
+        putchar('\n');
+        return (0);
+}
