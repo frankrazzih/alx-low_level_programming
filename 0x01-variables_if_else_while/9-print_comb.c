41e9667ff5e7b9be7111c35039b5cac860a28eb0
Francis Chege <francischege602@gmail.com>
2023-06-16 15:20:28 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 	/*set the range of the first no I want to combine*/
 	for (num1 =0; num1 <= 9; num1++){
 		/*set the range of the second no.*/
-	for (num2 =0; num2 <= 9; num2++){
+/*	for (num2 =0; num2 <= 9; num2++){*/
 		/*print the combination*/
 	putchar(num1 + '0');
-	putchar(num2 + '0');
+/*	putchar(num2 + '0');*/
 	putchar(',');
 	putchar(' ');
-	}}
+	}
 	/*end*/
 return (0);
 }
