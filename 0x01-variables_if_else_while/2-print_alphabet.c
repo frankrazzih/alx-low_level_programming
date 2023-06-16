16e9a52c05064087489586d68855b6310995d172
Francis Chege <francischege602@gmail.com>
2023-06-16 12:34:58 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * description - print a -z
+ * return - 0(success)
+ */
+int main(void){
+char alpha = 'a';
+char omega = 'z';
+	while (alpha <= omega){
+		putchar(alpha);
+			alpha++;
+		putchar('\n');
+	}
+	return (0);			
+}
