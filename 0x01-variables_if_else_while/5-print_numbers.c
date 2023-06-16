59961da3f109f8498f2b3c1019d38c9dc43a855a
Francis Chege <francischege602@gmail.com>
2023-06-16 13:43:56 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * description - print a -z
+ * return - 0(success)
+ */
+int main(void){
+int alpha = 0;
+int omega = 9;
+        while (alpha <= omega){
+                        printf("%d",alpha);
+                        alpha++;
+                        }
+        putchar('\n');
+        return (0);
+}
