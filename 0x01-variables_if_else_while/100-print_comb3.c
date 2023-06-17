fabd895204996f4d7b9ff06ab535e18c52e3e6d0
Francis Chege <francischege602@gmail.com>
2023-06-17 11:25:28 -0700

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * Main - entry
+ * Description - print possible combination of two digits
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	/*initialize variables*/
+	int num1;
+	int num2;
+	/*loop to iterate over single digits*/
+	for (num1 =0; num1 <=9; num1++)
+	{
+		for (num2 =0; num2 <=9; num2++)
+		{
+			/*skip the same no.s*/
+			if (num1 == num2)
+			{
+				continue;
+			}
+			/*skip similar no combination*/
+			if (num2 < num1)
+			{
+				continue;
+			}
+			/*print the combination*/
+			putchar(num1+ '0');
+			putchar(num2+ '0');					}
+	}
+	/*program ends*/
+	return (0);
+}
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * main - entry
+ * Main - entry
  * description - print a -z
- * return - 0(success)
+ * return: 0(success)
  */
-int main(void){
-char alpha = 'a';
-char omega = 'z';
-	while (alpha <= omega){
-			putchar(alpha);
-			alpha++;
-			}
+int main(void)
+{
+	char alpha = 'a';
+	char omega = 'z';
+	while (alpha <= omega)
+	{
+		putchar(alpha);
+		alpha++;
+	}
 	putchar('\n');
 	return (0);			
 }
