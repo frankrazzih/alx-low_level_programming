56a0c585ef2e54d483133cfadbae528f5db6bfe1
Francis Chege <francischege602@gmail.com>
2023-06-17 11:40:00 -0700

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -26,7 +26,13 @@ int main(void)
 			}
 			/*print the combination*/
 			putchar(num1+ '0');
-			putchar(num2+ '0');					}
+			putchar(num2+ '0');
+			if (num1 != 8 || num2 != 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
 	}
 	/*program ends*/
 	return (0);
