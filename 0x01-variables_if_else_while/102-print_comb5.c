23af064bd84fdeb09c8beb83ff41d0de47defdfb
Francis Chege <francischege602@gmail.com>
2023-06-18 10:16:21 -0700

    advanced3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
-/**
- * Main - entry
- * Description - print possible combination of two digits
- * Return: Always 0(success)
- */
 int main(void)
 {
-        /*initialize variables*/
-        int num1;
-        int num2;
-	int num3;
-	int num4;
-        /*loop to iterate over single digits*/
-        for (num1 =0; num1 <=9; num1++)
-        {
-                for (num2 =0; num2 <=9; num2++)
-                {
-			for (num3 =0; num3 <=9; num3++)
+	int num1;
+	int num2;
+	int num11;
+	int num12;
+	int num22;
+	int num23;
+	for (num1=0; num1 <=99; num1++)
+	{
+		for (num2=0; num2 <=99; num2++)
+		{
+			if (num1 == num2 || num1>num2 )
 			{
-				for (num4 =0; num4 <=9; num4++)
-				{
-                        /*skip the same no.s*/
-                        if (num1 == num2 || num1 == num3 || num1 == num4 || num2 == num3 || num2 == num4 || num3 == num4 || num2 < num1 || num3 < num2 || num4 < num3)
-                        {
-                                continue;
-                        }
-                        /*print the combination*/
-                        putchar(num1+ '0');
-			putchar(num2+ '0');
+				continue;
+			}
+			if (num2 == 99 && num1 == 98)
+			{
+				putchar('\n');
+			}
+			int num11=num1 / 10;
+			int num12=num1 % 10;
+			int num22=num2 / 10;
+			int num23=num2 % 10;
+			putchar(num11 + '0');
+			putchar(num12 + '0');
 			putchar(' ');
-                        putchar(num3+ '0');
-			putchar(num4+ '0');
-                        if (num1 != 6 || num2 != 7 || num3 != 8 || num4 != 9)
-                        {
-                        putchar(',');
-                        putchar(' ');
-			}
-				}
-			}
-                }
-        }
-        putchar('\n');
-        /*program ends*/
+			putchar(num22 + '0');
+			putchar(num23 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	return (0);
 }
