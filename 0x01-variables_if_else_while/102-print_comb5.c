78e3bb46bd977d2c2d30f63a06d6676d032876a9
Francis Chege <francischege602@gmail.com>
2023-06-19 11:40:10 -0700

    advanced3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,48 +10,48 @@
  */
 int main(void)
 {
-    int num1;
-    int num2;
-    int last = 0;
-    int num11;
-    int num12;
-    int num21;
-    int num22;
+	int num1;
+	int num2;
+	int last = 0;
+	int num11;
+	int num12;
+	int num21;
+	int num22;
 
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = num1 + 1; num2 <= 99; num2++)
-        {
-            if (num1 == 98 && num2 == 99)
-            {
-                last = 1;
-            }
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			if (num1 == 98 && num2 == 99)
+			{
+				last = 1;
+			}
 
-            /* Extracting digits from num1 and num2 */
-            num11 = num1 / 10;
-            num12 = num1 % 10;
-            num21 = num2 / 10;
-            num22 = num2 % 10;
+			/* Extract digits from num1 and num2 */
+			num11 = num1 / 10;
+			num12 = num1 % 10;
+			num21 = num2 / 10;
+			num22 = num2 % 10;
 
-            /* Printing the combination */
-            putchar(num11 + '0');
-            putchar(num12 + '0');
-            putchar(' ');
-            putchar(num21 + '0');
-            putchar(num22 + '0');
+			/* Print the combination */
+			putchar(num11 + '0');
+			putchar(num12 + '0');
+			putchar('	');
+			putchar(num21 + '0');
+			putchar(num22 + '0');
 
-            if (last)
-            {
-                putchar('\n');
-            }
-            else
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (last)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    return 0;
+	return (0);
 }
 
