7e34d8101cde61b9d4ede3edd910fa1aef65a862
Francis Chege <francischege602@gmail.com>
2023-06-19 11:28:53 -0700

    advanced3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,6 +5,10 @@ int main(void)
     int num1;
     int num2;
     int last=0;
+    int num11;
+    int num12;
+    int num21;
+    int num22;
     for (num1 = 0; num1 <= 99; num1++)
     {
         for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -17,10 +21,10 @@ int main(void)
 		{
 			putchar('\n');
 		}*/
-            int num11 = num1 / 10;
-            int num12 = num1 % 10;
-            int num21 = num2 / 10;
-            int num22 = num2 % 10;
+             num11 = num1 / 10;
+             num12 = num1 % 10;
+             num21 = num2 / 10;
+             num22 = num2 % 10;
             putchar(num11 + '0');
             putchar(num12 + '0');
             putchar(' ');
