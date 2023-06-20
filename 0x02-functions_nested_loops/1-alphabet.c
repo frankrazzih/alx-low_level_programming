ff33609939357389eebe677515a540e64ba51629
Francis Chege <francischege602@gmail.com>
2023-06-20 12:49:28 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-void print_alphabet(void);
+#include "main.h"
 void print_alphabet(void)
 {
     char alpha;
 
     for (alpha = 'a'; alpha <= 'z'; alpha++)
     {
-        putchar(alpha);
+        _putchar(alpha);
+	_putchar('\n');
     }
 }
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+int main()
 {
-	print_alphabet();
-	putchar('\n');
-	return (0);
+	return 0;
 }
