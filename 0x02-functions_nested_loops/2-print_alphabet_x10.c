4fe72e1fab5d8b818abf99cde8ff19b6d67835d8
Francis Chege <francischege602@gmail.com>
2023-06-20 09:30:07 -0700

    times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	return 0;
+}
+void print_alphabet_x10(void)
+{
+	int times=0;
+	while (times < 10)
+	{
+	char a='a';
+	for (a='a'; a <='z'; a++)
+	{
+		putchar(a);
+	}
+			putchar('\n');
+			times++;
+	}
+}
