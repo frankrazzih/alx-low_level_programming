4764929f48d0ec79801a5f8e7508440c019eaddb
Francis Chege <francischege602@gmail.com>
2023-06-20 14:24:14 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,17 +8,17 @@
  */
 int _isalpha(int c)
 {
-        int result;
+	int result;
 
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        {
-                result = 1;
-        }
-        else
-        {
-                result = 0;
-        }
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
 
-        return (result);
+	return (result);
 }
 
