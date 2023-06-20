ed9f6eb380711ca8936f3a613e8b49f550ff1f86
Francis Chege <francischege602@gmail.com>
2023-06-20 04:27:34 -0700

    function1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,25 +3,30 @@
 /**
  * print_alphabet - Prints the lowercase alphabet followed by a new line.
  */
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		putchar(alpha);
-	}
-}
+void print_alphabet(void);
 
 /**
- * main - Entry point of the program
+ * main - Entry point of the program.
  *
- * Return: 0 on successful execution
+ * Return: Always 0 (indicating success)
  */
 int main(void)
 {
-	print_alphabet();
-	putchar('\n');
-	return 0;
+    print_alphabet();
+    putchar('\n');
+    return 0;
+}
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ */
+void print_alphabet(void)
+{
+    char alpha;
+
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+        putchar(alpha);
+    }
 }
 
