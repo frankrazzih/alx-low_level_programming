c90712e3d54e49318c877ee1bf8a03c9a470b394
Francis Chege <francischege602@gmail.com>
2023-06-22 11:27:18 -0700

    avoid

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,17 @@
  */
 void print_most_numbers(void)
 {
-    int x;
+	int x;
 
-    for (x = 0; x <= 9; x++)
-    {
-        if (x == 2 || x == 4)
-        {
-            continue;
-        }
-        _putchar(x + '0');
-    }
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		_putchar(x + '0');
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
+
