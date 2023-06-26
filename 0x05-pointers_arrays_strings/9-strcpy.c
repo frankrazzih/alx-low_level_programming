475b4e6ef0489f53e17ba6fd82bee5a3ac3e34b6
Francis Chege <francischege602@gmail.com>
2023-06-26 15:20:24 -0700

    copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-        int x = strlen(src) + 1;
-        int y;
+	int x = strlen(src) + 1;
+	int y;
 
-        for (y = 0; y < x; y++)
-        {
-                dest[y] = src[y];
-        }
+	for (y = 0; y < x; y++)
+	{
+		dest[y] = src[y];
+	}
 
-        return dest;
+	return dest;
 }
 
