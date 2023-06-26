a28749a93114d55d11d6fb23cecdc821623cf15f
Francis Chege <francischege602@gmail.com>
2023-06-26 15:16:50 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string from source to destination
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int x = strlen(src) + 1;
+        int y;
+
+        for (y = 0; y < x; y++)
+        {
+                dest[y] = src[y];
+        }
+
+        return dest;
+}
+
