1d6866eed45659d4f416c23c4a60dd19bc8518d0
Francis Chege <francischege602@gmail.com>
2023-06-26 14:32:55 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,18 +13,18 @@
  */
 void print_array(int *a, int n)
 {
-    int m;
-    
-    for (m = 0; m < n; m++)
-    {
-        if (m == n - 1)
-        {
-            printf("%d\n", a[m]);
-        }
-        else
-        {
-            printf("%d, ", a[m]);
-        }
-    }
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		if (m == n - 1)
+		{
+			printf("%d\n", a[m]);
+		}
+		else
+		{
+			printf("%d, ", a[m]);
+		}
+	}
 }
 
