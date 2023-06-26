c8235756b8142e68a2961be7ad930810beea0df8
Francis Chege <francischege602@gmail.com>
2023-06-26 08:35:22 -0700

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+        size_t length = strlen(s);
+        return (int)length;
+}
+
