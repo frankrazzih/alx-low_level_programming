542cdd044ff6e2688f827a5f33e65266f93100b7
Francis Chege <francischege602@gmail.com>
2023-06-26 11:38:40 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The input string
+ *
+ * Description: This function prints each character of the input string
+ * in reverse order, starting from the last character.
+ */
+void print_rev(char *s)
+{
+    size_t x = strlen(s);
+
+    putchar(s[x - 1]);
+
+    char y;
+    for (x = strlen(s); x > 0; x--)
+    {
+        y = s[x - 1];
+        putchar(y);
+    }
+
+    putchar('\n');
+}
+
