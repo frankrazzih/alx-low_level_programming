97b707d6693cf6ae5e744a04a0a702acf87c4949
Francis Chege <francischege602@gmail.com>
2023-06-29 04:24:40 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - Concatenates two strings, up to a maximum of n characters from src.
+ * @dest: The string to which src will be appended.
+ * @src: The string that will be appended to dest.
+ * @n: The maximum number of characters to be appended from src.
+ *
+ * Return: Pointer to the resulting concatenated string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    size_t destlen = strlen(src);
+    int x;
+
+    for (x = 0; x < n && src[x] != '\0'; x++)
+    {
+        dest[destlen + x] = src[x];
+    }
+
+    dest[destlen + x] = '\0';
+
+    return (dest);
+}
+
