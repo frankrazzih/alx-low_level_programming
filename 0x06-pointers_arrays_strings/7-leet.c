26d5324190c592e14dd17785fcfd63e265ef427d
Francis Chege <francischege602@gmail.com>
2023-06-30 13:17:13 -0700

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,15 +14,25 @@ char *leet(char *str)
     for (x = 0; x < strlen(str); x++)
     {
         if (str[x] == 'a' || str[x] == 'A')
+        {
             str[x] = '4';
+        }
         else if (str[x] == 'e' || str[x] == 'E')
+        {
             str[x] = '3';
+        }
         else if (str[x] == 'o' || str[x] == 'O')
+        {
             str[x] = '0';
+        }
         else if (str[x] == 't' || str[x] == 'T')
+        {
             str[x] = '7';
+        }
         else if (str[x] == 'l' || str[x] == 'L')
+        {
             str[x] = '1';
+        }
     }
 
     return str;
