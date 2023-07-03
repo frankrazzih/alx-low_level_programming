3f87254773ebf4c79dd7ad8119927aebc891acb7
Francis Chege <francischege602@gmail.com>
2023-07-03 12:07:12 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sum of the main diagonal and anti-diagonal elements
+ *                   of a square matrix.
+ * @a: Pointer to the square matrix.
+ * @size: Size of the square matrix.
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int diag = 0;
+	int diag1 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		diag += a[(size + 1) * x];
+		diag1 += a[(size - 1) * (x + 1)];
+	}
+
+	/* Convert the sums to strings */
+	char diag_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
+	char diag1_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
+	int diag_sum_length = 0;
+	int diag1_sum_length = 0;
+	int temp = diag;
+
+	while (temp > 0)
+	{
+		diag_sum_str[diag_sum_length++] = '0' + (temp % 10);
+		temp /= 10;
+	}
+
+	if (diag == 0)
+		diag_sum_str[diag_sum_length++] = '0';
+
+	diag_sum_str[diag_sum_length] = '\0';
+	temp = diag1;
+
+	while (temp > 0)
+	{
+		diag1_sum_str[diag1_sum_length++] = '0' + (temp % 10);
+		temp /= 10;
+	}
+
+	if (diag1 == 0)
+		diag1_sum_str[diag1_sum_length++] = '0';
+
+	diag1_sum_str[diag1_sum_length] = '\0';
+
+	/* Print the sums on the same line, separated by comma and space */
+	int i;
+
+	for (i = 0; i < diag_sum_length; i++)
+		_putchar(diag_sum_str[diag_sum_length - 1 - i]);
+
+	_putchar(',');
+	_putchar(' ');
+
+	for (i = 0; i < diag1_sum_length; i++)
+		_putchar(diag1_sum_str[diag1_sum_length - 1 - i]);
+
+	_putchar('\n');
+}
+
