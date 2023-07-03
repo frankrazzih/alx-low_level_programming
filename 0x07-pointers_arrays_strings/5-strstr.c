7b376b2fe028a2aee83ee9f551bc81a55a55e14d
Francis Chege <francischege602@gmail.com>
2023-07-03 05:47:44 -0700

    str

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - checks for the occurence of substring in a string
+ * @haystack : string being checked
+ * @needle : substring being searched for
+ * Return: pointer to occurence of substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	haystack = strstr(haystack, needle);
+	return (needle);
+}
