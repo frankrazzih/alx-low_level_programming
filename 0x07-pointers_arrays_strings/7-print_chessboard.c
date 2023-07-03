f764ac7efa296d4d9310c33da388eb09986565ca
Francis Chege <francischege602@gmail.com>
2023-07-03 08:11:00 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,11 +7,11 @@
 void print_chessboard(char (*a)[8])
 {
 	int x, y;
-	for (x = 0;x < 8;x++)
+
+	for (x = 0; x < 8; x++)
 	{
-		for (y = 0;y < 8;y++)
+		for (y = 0; y < 8; y++)
 		{
-			
 			_putchar(a[x][y]);
 			if (y == 7)
 			{
