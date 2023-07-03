0a54694ed8cee681df774b5444bdbc323c0eebf4
Francis Chege <francischege602@gmail.com>
2023-07-03 05:00:55 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,10 +4,10 @@
  * _strchr - returns a pointer with the first occurence of c in a string
  * @s : pointer to the string
  * @c : the character being checked for in a string
- * Return : s which is a pointer to the first ocuur of c in string
+ * Return: s which is a pointer to the first ocuur of c in string
  */
 char *_strchr(char *s, char c)
 {
-	s = strchr(s,c);
+	s = strchr(s, c);
 	return (s);
 }
