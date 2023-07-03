5d0865bb1146eb608844ddfec0d794849aba22fb
Francis Chege <francischege602@gmail.com>
2023-07-03 12:34:03 -0700

    diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,22 +9,20 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x;
+	int x, i;
 	int diag = 0;
 	int diag1 = 0;
-
+        char diag_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
+        char diag1_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
+        int diag_sum_length = 0;
+        int diag1_sum_length = 0;
+        int temp = diag;
+	
 	for (x = 0; x < size; x++)
 	{
 		diag += a[size * x + x];
 		diag1 += a[size * (x + 1) - (x + 1)];
 	}
-
-	/* Convert the sums to strings */
-	char diag_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
-	char diag1_sum_str[12]; /* Assuming the maximum sum can be represented with 11 digits */
-	int diag_sum_length = 0;
-	int diag1_sum_length = 0;
-	int temp = diag;
 	while (temp > 0)
 	{
 		diag_sum_str[diag_sum_length++] = '0' + (temp % 10);
@@ -42,9 +40,6 @@ void print_diagsums(int *a, int size)
 	if (diag1 == 0)
 		diag1_sum_str[diag1_sum_length++] = '0';
 	diag1_sum_str[diag1_sum_length] = '\0';
-
-	/* Print the sums on the same line, separated by comma and space */
-	int i;
 	for (i = 0; i < diag_sum_length; i++)
 		_putchar(diag_sum_str[diag_sum_length - 1 - i]);
 	_putchar(',');
