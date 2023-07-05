5fa12e79acc740174fb56c9d4367b5400f450633
Francis Chege <francischege602@gmail.com>
2023-07-05 06:12:07 -0700

    recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/** _puts_recursion - outputs a string followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
