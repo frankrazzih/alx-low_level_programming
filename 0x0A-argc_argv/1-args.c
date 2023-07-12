eecdf30e2bf5a837d04fe867e4fe445a3c2970a1
Francis Chege <francischege602@gmail.com>
2023-07-12 14:04:12 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program starting point
+ * @argc : number of arguments passed
+ * @argv : pointer to the arguments passed
+ * Return: 0 indicating success
+ */
+int main(int argc, char *argv[])
+{
+	int x = argc - 1;
+
+	printf("%d\n", x);
+	return (0);
+}
