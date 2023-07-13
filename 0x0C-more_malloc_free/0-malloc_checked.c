88eb4bd532e6f5c51a8ad5e4580dbcb3ec97e0a8
Francis Chege <francischege602@gmail.com>
2023-07-13 03:22:51 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b - the size of memory to allocate
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+}
