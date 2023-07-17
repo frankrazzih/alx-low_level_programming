38cf1d8893081c81961adfddf744d4496554cde8
Francis Chege <francischege602@gmail.com>
2023-07-17 12:08:02 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - contains details of a dog
+ * @name : name of the dog
+ * @age : age of the dog
+ * @owner : name of dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
