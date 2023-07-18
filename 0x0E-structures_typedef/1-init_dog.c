4ebd6ca3d9d4d4ec02cac1c2e6afde4ead828209
Francis Chege <francischege602@gmail.com>
2023-07-17 18:33:34 -0700

    doh

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,11 @@
+#include "main.h"
+#include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	struct dog d
+	{
+		char name;
+		float age;
+		char owner;
+	};
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * struct dog - contains details of a dog
- * @name : name of the dog
- * @age : age of the dog
- * @owner : name of dog owner
- */
+  * struct dog - struct
+  * @name: first
+  * @age: second
+  * @owner: third
+  * Description: Struct
+  */
 
 struct dog
 {
@@ -14,5 +15,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
