8192ab15f7c9461237f78254c8a6b16979497177
Francis Chege <francischege602@gmail.com>
2023-07-29 02:21:11 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,46 @@
-#include "main.h"
-#include <math.h>
+ #include "main.h"
+
 /**
- * _sqrt_recursion - returns the squareroot of a number
- * @n : input number
+ * nat - returns  natural square root of a number.
+ * @n: number.
+ * @x: iterator.
+ * Return: square root or -1.
+ */
+
+int nat(int n, int x)
+{
+	if (x % (n / x) == 0)
+	{
+		if (x * (n / x) == n)
+		{
+			return (x);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (0 + nat(n, x + 1));
+}
+/**
+ * _sqrt_recursion - return square root
+ * @n: number to whose sqrt is needed
  * Return: square root of n
  */
+
 int _sqrt_recursion(int n)
 {
-	float x = sqrt(n);
-	int y = (int) x;
-
-	if (x == y)
-	{
-		return (y);
-	}
-	else
+	if (n < 0)
 	{
 		return (-1);
 	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	return (nat(n, 2));
 }
