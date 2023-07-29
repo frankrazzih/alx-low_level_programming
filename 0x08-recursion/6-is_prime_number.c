e268fb3b85573d8169d3bf7281d0eb42468d5a27
Francis Chege <francischege602@gmail.com>
2023-07-29 02:53:07 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,49 @@
 #include "main.h"
-int is_prime_number(int n)
+/**
+ * prime - returns a prime number
+ * @n: number
+ * @x: no to iterate
+ * Return: prime no
+ */
+int prime(unsigned int n, unsigned int x)
 {
-	int x = 1;
-	if (n > 1 && n)
+	if (n % x == 0)
 	{
+		if (n == x)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 	else
 	{
+		return (0 + prime(n, x + 1));
+	}
+}
+/**
+ * is_prime_number - checks whether a no is prime
+ * @n : no to check
+ * Return: 0 if not a prime no else return 1
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+	{
 		return (0);
+	}
+	else if (n < 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, 2));
 	}
 }
