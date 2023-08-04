290caac037b4a2729ceeb0798cdf82ab3c905ec2
Francis Chege <francischege602@gmail.com>
2023-08-04 05:06:53 -0700

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,12 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (-1);
 	}
-
-	return ((n & (1UL << index)) ? 1 : 0);
+	if (n & (1UL << index))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
