610b32f0addbed95f6eadc09352bb92303a9aeed
frank chege <francischege602@gmail.com>
2023-08-31 23:49:22 +0300

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,5 +13,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
 
 #endif
