8606725d7fd2e9bcfc4ae69a5c5e5c8bdaaa9593
frank chege <francischege602@gmail.com>
2023-10-12 13:22:39 +0300

    doubly lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_t-pointer to 1st node
+ * Return: no. of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int counter = 0;
+	const dlistint_t *cur = h;
+
+	while(cur != NULL)
+	{
+		printf("%d\n", cur->n);
+		counter++;
+		cur = cur->next;
+	}
+	return (counter);
+}
